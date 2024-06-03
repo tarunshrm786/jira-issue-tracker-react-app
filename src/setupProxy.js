f01eb01350cbf71fb6f5c24d87e6051810acac0e
Tarun Sharma <tarunshrm768@gmail.com>
2024-06-03 15:57:25 +0530

    commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -14,7 +14,8 @@ require('dotenv').config();
 
 module.exports = function(app) {
   app.use('/api', createProxyMiddleware({
-    target: process.env.REACT_APP_JIRA_API_URL,
+    // target: process.env.REACT_APP_JIRA_API_URL,
+    target: 'https://tarunshrm768.atlassian.net',
     changeOrigin: true,
     pathRewrite: {
       '^/api': '/rest/api/3' // Rewrite '/api' to '/rest/api/3' in the request URL
