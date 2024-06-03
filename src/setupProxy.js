678ab8c450fac3444e5858eab9aa57a6ed7934f6
Tarun Sharma <tarunshrm768@gmail.com>
2024-06-03 15:45:00 +0530

    update proxy file commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -8,18 +8,20 @@
 //   }));
 // };
 
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
 require('dotenv').config();
 
 module.exports = function(app) {
+  // Proxy for localhost
   app.use('/api', createProxyMiddleware({
-    target: 'https://tarunshrm768.atlassian.net', // Update with the correct Jira API base URL
+    target: process.env.REACT_APP_JIRA_API_URL,
     changeOrigin: true,
-    pathRewrite: {
-      '^/api': '/api/rest/api/3', // Rewrite '/api' to '/api/rest/api/3' in the proxy request
-    },
-    headers: {
-      Authorization: `Basic ${Buffer.from(process.env.REACT_APP_JIRA_USERNAME + ':' + process.env.REACT_APP_JIRA_PASSWORD).toString('base64')}`,
-    },
+  }));
+
+  // Proxy for Netlify app
+  app.use('/netlify-api', createProxyMiddleware({
+    target: 'https://superlative-pony-59485f.netlify.app',
+    changeOrigin: true,
   }));
 };
