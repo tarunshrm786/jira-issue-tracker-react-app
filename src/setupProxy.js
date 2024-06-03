4fd2c3ee2e91a3a7a6943c160299945a8fe30c1b
Tarun Sharma <tarunshrm768@gmail.com>
2024-06-03 15:25:41 +0530

    update proxy file

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,25 @@
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+// require('dotenv').config();
+
+// module.exports = function(app) {
+//   app.use('/api', createProxyMiddleware({
+//     target: process.env.REACT_APP_JIRA_API_URL,
+//     changeOrigin: true,
+//   }));
+// };
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
 require('dotenv').config();
 
 module.exports = function(app) {
   app.use('/api', createProxyMiddleware({
-    target: process.env.REACT_APP_JIRA_API_URL,
+    target: 'https://tarunshrm768.atlassian.net', // Update with the correct Jira API base URL
     changeOrigin: true,
+    pathRewrite: {
+      '^/api': '/api/rest/api/3', // Rewrite '/api' to '/api/rest/api/3' in the proxy request
+    },
+    headers: {
+      Authorization: `Basic ${Buffer.from(process.env.REACT_APP_JIRA_USERNAME + ':' + process.env.REACT_APP_JIRA_PASSWORD).toString('base64')}`,
+    },
   }));
 };
