94cdb10c4b8f349f234451b315fef1e705a75d77
Tarun Sharma <tarunshrm768@gmail.com>
2024-06-03 15:49:54 +0530

    proxy file commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -13,16 +13,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 require('dotenv').config();
 
 module.exports = function(app) {
-  // Proxy for localhost
   app.use('/api', createProxyMiddleware({
     target: process.env.REACT_APP_JIRA_API_URL,
     changeOrigin: true,
-  }));
-
-  // Proxy for Netlify app
-  app.use('/netlify-api', createProxyMiddleware({
-    // target: 'https://superlative-pony-59485f.netlify.app',
-    target: process.env.REACT_APP_JIRA_API_URL,
-    changeOrigin: true,
+    pathRewrite: {
+      '^/api': '/rest/api/3' // Rewrite '/api' to '/rest/api/3' in the request URL
+    }
   }));
 };
